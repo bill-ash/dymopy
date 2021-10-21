fc805f087a11e2755e5136673daaaa732253accb
bill-ash <bill@overco.net>
2021-10-21 12:36:41 -0400

    Update example app.

--- examples/input.py
+++ examples/input.py
@@ -4,11 +4,13 @@ from dymopy import Dymo
 from dymopy.client import make_params
 
 base_url = 'http://localhost:3000'
+
 body = {
     'top': 'hello', 
-    'bottom': 'world',
-    'copies': str(1),
-    'side': 'Left'}
+    'bottom': 'rob with copies',
+    'copies': str(3),
+    'side': 'Left'
+    }
 
 resp = requests.post(base_url, params=body)
 resp.json()
